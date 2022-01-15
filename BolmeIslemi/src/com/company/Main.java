bbcaa75a489838dcd77752d2392650838478269f
gizemdemirtaass <79507052+gizemdemirtaass@users.noreply.github.com>
2022-01-15 16:54:21 +0300

    Add files via upload

---
+++ BolmeIslemi/src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main (String[] args){
+        int bolunen = 25;
+        int bolen = 4;
+
+        int bolum = bolunen/bolen;
+        int kalan = bolunen % bolen;
+
+        System.out.println("Bölüm : "+ bolum);
+        System.out.println("Kalan : "+kalan);
+    }
+}
